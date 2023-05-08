e71ddcbba9df8c538bec5d72ee49b11f113f429d
Cris Sidoly <cristhiansidoly@gmail.com>
2023-05-08 15:33:22 -0300

    toggle likes e nada acontece com a foto já com like

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,74 +1,10 @@
 import { useState } from "react"
 
 
-// export default function Post({plim}) {
-//   const [save, setSave] = useState(false);
-//   const [isLiked, setIsLiked] = useState(false);
-//   const [likes, setLikes] = useState(plim.likes);
-
-//   const handleLikeClick = (event) => {
-//     setLikes(plim.likes + 1);
-//     setIsLiked(!isLiked);
-//   };
-
-  
-//     return (
-//         <div data-test="post" class="post">
-//             <div class="topo">
-//             <div class="usuario">
-//                 <img src={plim.fotoUser} alt="meowed"/>
-//                 meowed
-//             </div>
-//             <div class="acoes">
-//                 <ion-icon name="ellipsis-horizontal"></ion-icon>
-//             </div>
-//             </div>
-
-//         <div class="conteudo">
-//         <img 
-//           data-test="post-image" 
-//           onClick={handleLikeClick} 
-//           src={plim.fotoPostada} 
-//           alt="gato-telefone"
-//         />
-//         </div>
-
-//         <div class="fundo">
-//           <div class="acoes">
-//             <div>
-//             <ion-icon 
-//               data-test="like-post"
-//               name={isLiked ? "heart" : "heart-outline"} 
-//               onClick={() => setIsLiked(!isLiked)}  
-//               style={{ color: isLiked ? "red" : "black" }}
-//             />
-//               <ion-icon name="chatbubble-outline"></ion-icon>
-//               <ion-icon name="paper-plane-outline"></ion-icon>
-//             </div>
-//             <div data-test="save-post"
-//               onClick={() => {setSave(!save)}}
-//               style={{ color: save ? "black" : "" }}>
-//               {!save ? 
-//               <ion-icon data-test="save-post" name="bookmark-outline"></ion-icon> : <ion-icon  name="bookmark"></ion-icon>}
-//             </div>
-//           </div>
-
-//           <div class="curtidas">
-//             <img src={plim.fotoCurtidor} alt="respondeai"/>
-//             <div class="texto" data-test="like-post">
-//               Curtido por <strong>{plim.curtidoPor}</strong> e <strong>outras {plim.likes} pessoas</strong>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-
-//     )
-// }
-
 export default function Post({ plim }) {
   const [save, setSave] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
-  const [likes, setLikes] = useState(plim.likes);
+  const [likes, setLikes] = useState(parseInt(plim.likes));
 
   const handleLikeClick = (event) => {
     if (!isLiked) {
@@ -77,6 +13,13 @@ export default function Post({ plim }) {
       setLikes(likes - 1);
     }
     setIsLiked(!isLiked);
+  };
+
+  const handlePostClick = (event) => {
+    if (!isLiked) {
+      setLikes(likes + 1);
+    }
+    setIsLiked(true);
   };
 
   return (
@@ -94,7 +37,7 @@ export default function Post({ plim }) {
       <div class="conteudo">
         <img
           data-test="post-image"
-          onClick={handleLikeClick}
+          onClick={handlePostClick}
           src={plim.fotoPostada}
           alt="gato-telefone"
         />
